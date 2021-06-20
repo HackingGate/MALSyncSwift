a292d33a5c3fb71bc54c620ad533140811e4c382
HackingGate <i@hackinggate.com>
2021-06-20 11:33:48 +0900

    SwiftLint (#1)
    * new file:   .github/workflows/swiftlint.yml
    
    * modified:   README.md
    
    * SwiftLint auto-correction

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "MALSyncSwift",
-            targets: ["MALSyncSwift"]),
+            targets: ["MALSyncSwift"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -24,6 +24,6 @@ let package = Package(
             path: "Sources/MALSyncSwift"),
         .testTarget(
             name: "MALSyncSwiftTests",
-            dependencies: ["MALSyncSwift"]),
+            dependencies: ["MALSyncSwift"])
     ]
 )
